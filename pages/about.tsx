6f13f89775b5df41ede8afd593a395e9359a204f
nhan.phan <hieunhan2000@gmail.com>
2023-05-07 15:58:18 +0700

    update 0207

--- pages/about.tsx
+++ pages/about.tsx
@@ -1,7 +1,16 @@
+import { useRouter } from 'next/router'
 import React from 'react'
 
 export interface AboutPageProps{}
 
 export default function AboutPage(props: AboutPageProps){
+    const router = useRouter()
+    console.log('About query: ', router.query)
     return <div>About Page</div>
+}
+
+export function getServerSideProps(){
+    return {
+        props: {}, 
+    }
 }
--- pages/posts/index.tsx
+++ pages/posts/index.tsx
@@ -1,7 +1,43 @@
-import React from 'react'
+import { GetStaticProps, GetStaticPropsContext } from "next";
+import Link from "next/link";
+import React from "react";
 
-export interface PostListPageProps{}
-
-export default function PostListPage(props: PostListPageProps){
-    return <div>Post List Page</div>
+export interface PostListPageProps {
+  posts: any[];
 }
+
+export default function PostListPage({ posts }: PostListPageProps) {
+  return (
+    <div>
+      <h1>Post List Page</h1>
+      <ul>
+        {posts.map((post) => (
+          <li key={post.id}>
+            <Link href={`/posts/${post.id}`}>{post.title}</Link>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+export const getStaticProps: GetStaticProps<PostListPageProps> = async (
+  context: GetStaticPropsContext
+) => {
+  // server-side
+  // build-time
+
+  console.log("static props");
+
+  const response = await fetch(
+    "https://js-post-api.herokuapp.com/api/posts?_page=1"
+  );
+  const data = await response.json();
+  // console.log(data)
+
+  return {
+    props: {
+      posts: data.data.map((x: any) => ({ id: x.id, title: x.title })),
+    },
+  };
+};
