d76f7e40317962310fc257bc34783972dd904d80
Nhan Phan <nhan.phan@s3corp.com.vn>
2023-12-05 21:41:50 +0700

    Server Side Rendering

---
+++ pages/posts/params-cache-stale.tsx
@@ -0,0 +1,43 @@
+import { GetServerSidePropsContext } from "next"
+import { useRouter } from "next/router"
+import { useEffect, useState } from "react"
+
+export interface ParamsPageProps {
+    query: any
+    post: any
+}
+
+
+export default function ParamsPage({query, post}: ParamsPageProps){
+    const router = useRouter()
+    const [second, setSeconds] = useState(0)
+    
+    useEffect(() => {
+        const intervalID = setInterval(()=>{
+            setSeconds((x) => {
+                if (x > 60) clearInterval(intervalID)
+                return x + 1
+            })
+        }, 1000)
+        return () => clearInterval(intervalID)
+    }, [])
+}
+
+export async function getServerSideProps(context: GetServerSidePropsContext){
+    context.res.setHeader('Cache-Control', 's-maxage=5, stale-while-revalidate')
+    await new Promise((res) => setTimeout(res, 3000))
+
+    const postId = context.query.postId
+    if (!postId) return {
+        props: {query: context.query}
+    }
+
+    const response = await fetch(`https://js-post-api.herokuapp.com/api/posts/${postId}`)
+    const data = await response.json()
+    return {
+        props: {
+            query: context.query,
+            post: data,
+        }
+    }
+}
